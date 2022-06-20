6599bed42b1fe3ca862fe3331a4e900593e239d3
b3nk3i <rom1.petit@gmail.com>
2022-06-20 19:17:30 +0200

    clean dep

--- forex-mtl/README.md
+++ forex-mtl/README.md
@@ -3,7 +3,9 @@
 - The rate should not be older than 5 minutes
 - The service should support at least 10,000 successful requests per day with 1 API token
   - 9 currencies are supported: for each currency 9 currencies x 8 pairs x 2 direction (e.g. JPY/AUD, AUD/JPY) = 77 x 2 = 144 possible pairs
-  - refresh rate for all pair cache every 2 minutes => 740 request / day 
+  - refresh rate for all pair cache every 2 minutes => 740 request / day
+- some integration tests requires docker running
+
 test one frame:
 ```
 curl -H "token: 10dc303535874aeccc86a8251e6992f5" "localhost:8080/rates?from=USD&to=JPY"
--- forex-mtl/project/Dependencies.scala
+++ forex-mtl/project/Dependencies.scala
@@ -9,6 +9,7 @@ object Dependencies {
     val http4s     = "0.23.12"
     val circe      = "0.13.0"
     val pureConfig = "0.17.1"
+    val tsec       = "0.4.0"
 
     val kindProjector  = "0.10.3"
     val logback        = "1.2.3"
@@ -16,6 +17,8 @@ object Dependencies {
     val scalaCheck     = "1.15.3"
     val scalaTest      = "3.2.7"
     val catsScalaCheck = "0.3.0"
+    val docker         = "0.40.8"
+    val scalatestplus  = "3.1.0.0-RC2"
   }
 
   object Libraries {
@@ -30,7 +33,7 @@ object Dependencies {
     lazy val http4sServer    = http4s("http4s-blaze-server")
     lazy val http4sClient    = http4s("http4s-blaze-client")
     lazy val http4sCirce     = http4s("http4s-circe")
-    lazy val tsec            = "io.github.jmcardon" %% "tsec-http4s" % "0.4.0"
+    lazy val tsec            = "io.github.jmcardon" %% "tsec-http4s" % Versions.tsec
     lazy val circeCore       = circe("circe-core")
     lazy val circeGeneric    = circe("circe-generic")
     lazy val circeGenericExt = circe("circe-generic-extras")
@@ -48,8 +51,8 @@ object Dependencies {
     lazy val scalaTest      = "org.scalatest"     %% "scalatest"                      % Versions.scalaTest
     lazy val scalaCheck     = "org.scalacheck"    %% "scalacheck"                     % Versions.scalaCheck
     lazy val catsScalaCheck = "io.chrisdavenport" %% "cats-scalacheck"                % Versions.catsScalaCheck
-    lazy val scalaTestPlus  = "org.scalatestplus" %% "scalatestplus-scalacheck"       % "3.1.0.0-RC2"
-    lazy val docker         = "com.dimafeng"      %% "testcontainers-scala-scalatest" % "0.40.7"
+    lazy val scalaTestPlus  = "org.scalatestplus" %% "scalatestplus-scalacheck"       % Versions.scalatestplus
+    lazy val docker         = "com.dimafeng"      %% "testcontainers-scala-scalatest" % Versions.docker
 
     val All: Seq[ModuleID] = Seq(
       compilerPlugin(Libraries.kindProjector),
