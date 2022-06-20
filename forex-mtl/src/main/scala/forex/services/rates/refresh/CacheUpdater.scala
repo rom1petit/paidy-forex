22b236cbb8226d35bd9505bf1b5eca8c8120d82f
b3nk3i <rom1.petit@gmail.com>
2022-06-20 02:53:14 +0200

    add missing log

--- forex-mtl/src/main/scala/forex/services/rates/refresh/CacheUpdater.scala
+++ forex-mtl/src/main/scala/forex/services/rates/refresh/CacheUpdater.scala
@@ -2,16 +2,19 @@ package forex.services.rates.refresh
 
 import cats.data.EitherT
 import cats.effect.Async
-import cats.implicits.{catsSyntaxApply, toFunctorOps, toTraverseOps}
+import cats.implicits.{ catsSyntaxApply, toFunctorOps, toTraverseOps }
 import forex.services.rates.errors
-import forex.services.rates.interpreters.{OneFrameCache, OneFrameLive}
+import forex.services.rates.interpreters.OneFrameLive.log
+import forex.services.rates.interpreters.{ OneFrameCache, OneFrameLive }
 
 import scala.concurrent.duration.DurationInt
 
 class CacheUpdater[F[_]: Async](oneFrameLive: OneFrameLive[F], oneFrameCache: OneFrameCache[F]) {
 
   def fill(): F[Either[errors.Error, Unit]] =
-    EitherT(oneFrameLive.list()).semiflatTap(rates => rates.traverse(oneFrameCache.put)).void.value
+    log("Update cache") {
+      EitherT(oneFrameLive.list()).semiflatTap(rates => rates.traverse(oneFrameCache.put)).void.value
+    }
 
   def update(): fs2.Stream[F, Unit] =
     fs2.Stream.repeatEval(fill() *> Async[F].sleep(2.minutes))
